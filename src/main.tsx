2ffd3f657bb125111512d46494256af075aae279
Del <wjg12341234@163.com>
2024-05-16 14:33:28 +0800

    fix: import path of App

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { WorkFlow } from "./app.tsx";
+import { WorkFlow } from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
