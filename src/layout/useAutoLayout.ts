83eb030deb8184a834c7812cd71f4342479ee172
Del Wang <wjg12341234@163.com>
2025-10-12 12:04:31 +0800

    refactor: improve layout handling in useAutoLayout

--- src/layout/useAutoLayout.ts
+++ src/layout/useAutoLayout.ts
@@ -3,71 +3,59 @@ import { useState } from "react";
 import { nextTick } from "@/utils/base";
 
 import { getReactflowData, kReactflow } from "../states/reactflow";
-import { getRootNode } from "./metadata";
-import { ILayoutReactflow, layoutReactflow } from "./node";
+import { type ILayoutReactflow, layoutReactflow } from "./node";
 
 const layoutWithFlush = async (options: ILayoutReactflow) => {
-  const layout = await layoutReactflow(options);
-  kReactflow.instance?.setNodes(layout.nodes);
-  kReactflow.instance?.setEdges(layout.edges);
+	const layout = await layoutReactflow(options);
 
-  // Check if the node has been correctly measured. Adjust the condition as needed based on your use case.
-  const isMeasured = () => !!kReactflow.instance?.getNodes()[0]?.measured;
-  while (!isMeasured()) {
-    await nextTick(10);
-  }
+	// Wait for the nodes and edges to be cleared
+	kReactflow.instance?.setNodes([]);
+	kReactflow.instance?.setEdges([]);
+	while (kReactflow.instance!.getNodes().length > 0) {
+		await nextTick(3);
+	}
 
-  const { nodes, edges } = getReactflowData();
-  return { layout, nodes, edges };
+	// Wait for the nodes and edges to be measured
+	kReactflow.instance?.setNodes(layout.nodes);
+	kReactflow.instance?.setEdges(layout.edges);
+	while (!kReactflow.instance?.getNodes()[0]?.measured) {
+		await nextTick(3);
+	}
+
+	// Get the layout data
+	const { nodes, edges } = getReactflowData();
+	return { layout, nodes, edges };
 };
 
 export const useAutoLayout = () => {
-  const [isDirty, setIsDirty] = useState(false);
+	const [isDirty, setIsDirty] = useState(false);
 
-  const layout = async (options: ILayoutReactflow) => {
-    if (!kReactflow.instance || isDirty || options.nodes.length < 1) {
-      return;
-    }
+	const layout = async (options: ILayoutReactflow) => {
+		if (!kReactflow.instance || isDirty || options.nodes.length < 1) {
+			return;
+		}
 
-    setIsDirty(true);
-    // Perform the first layout to measure node sizes
-    const firstLayout = await layoutWithFlush({
-      ...options,
-      visibility: "hidden", // Hide layout during the first layout pass
-    });
-    // Perform the second layout using actual node sizes
-    const secondLayout = await layoutWithFlush({
-      visibility: "visible",
-      ...options,
-      nodes: firstLayout.nodes ?? options.nodes,
-      edges: firstLayout.edges ?? options.edges,
-    });
-    setIsDirty(false);
+		setIsDirty(true);
+		// Perform the first layout to measure node sizes
+		const firstLayout = await layoutWithFlush({
+			...options,
+			visibility: "hidden", // Hide layout during the first layout pass
+		});
+		// Perform the second layout using actual node sizes
+		const secondLayout = await layoutWithFlush({
+			visibility: "visible",
+			...options,
+			nodes: firstLayout.nodes,
+			edges: firstLayout.edges,
+		});
+		setIsDirty(false);
 
-    // Center the viewpoint to the position of the root node
-    const root = getRootNode(secondLayout.layout.nodes);
-    // Give it a little offset so it's visually centered
-    const offset =
-      options.direction === "horizontal"
-        ? {
-            x: 0.2 * document.body.clientWidth,
-            y: 0 * document.body.clientHeight,
-          }
-        : {
-            x: 0 * document.body.clientHeight,
-            y: 0.3 * document.body.clientHeight,
-          };
-    if (root) {
-      kReactflow.instance.setCenter(
-        root.position.x + offset.x,
-        root.position.y + offset.y,
-        {
-          zoom: 1,
-        }
-      );
-    }
-    return secondLayout.layout;
-  };
+		// Center the viewpoint
+		kReactflow.instance.fitView();
+		kReactflow.instance.zoomTo(kReactflow.instance.getZoom() / 1.5);
 
-  return { layout, isDirty };
+		return secondLayout.layout;
+	};
+
+	return { layout, isDirty };
 };
